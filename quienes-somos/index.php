aa15b628086d22b9a307bec0c8974de5b8453c30
Guillef33 <guillef33@gmail.com>
2021-06-01 10:14:50 -0300

    cambio nombre estilos

--- quienes-somos/index.php
+++ quienes-somos/index.php
@@ -8,7 +8,7 @@
     <!-- <link rel="stylesheet" href="estilos.css"> -->
     <!--  <link rel="stylesheet" href="media.css">  -->
     <link rel="stylesheet" href="../estilos.css">
-    <link rel="stylesheet" href="../media-version2.css">
+    <link rel="stylesheet" href="../estilos-mobile.css">
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
